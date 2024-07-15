5a161b897079ee2cde462f485c11db952aa462c5
lioba00700 <lioba00700@naver.com>
2024-07-16 08:29:49 +0900

    연결전

--- src/screens/analyze.js
+++ src/screens/analyze.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './analyze.css'
 import { useNavigate } from 'react-router';
 
 const Analyze = () => {
 
   const navigate = useNavigate();
+  const [imageView, setImageView] = useState(null);
 
   const handle_submit = (event) => {
         event.preventDefault(); // 버튼 잠시 금지
@@ -28,15 +30,32 @@ const Analyze = () => {
     
         console.log('분석하기');
       };
+
+      const handle_Image = (event) => {
+        const file = event.target.files[0];
+          if (file) {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setImageView(reader.result);
+          };
+        reader.readAsDataURL(file);
+    }
+      };
+
   return (
     <div className="App">
       <div className='header'>
         
       </div>
-      <h1 style={{fontSize:'40px', fontWeight:'bold', color:'#F1A7A7', marginBottom:'20px', textAlign:'center'}}>대화 내용 분석</h1>
+      <h1 className="title" style={{fontSize:'40px', fontWeight:'bold', color:'#F1A7A7', marginBottom:'10px', textAlign:'center'}}>대화 내용 분석</h1>
       <div className="content_container">
+        <div className='question_box' style={{marginRight:'60px'}}>
+          <label>질문</label>
+          <input type='text' name='question'/>
+        </div>
         
         <form onSubmit={handle_submit} className='form_content'>
+          
             <div className='left'>
             <label>그 사람과의 관계</label>
             <input type='text' name='relation'/>
@@ -52,7 +71,8 @@ const Analyze = () => {
             <input type='range' name='term'/>
             <label>이미지 업로드</label>
             <div style={{}}>
-              <input type='file' name='upload' style={{height: '200px', alignContent:'center', fontSize:'0px', color:'white'}}/>
+              <input type='file' name='upload' style={{height: '200px', alignContent:'center', fontSize:'0px'}}  onChange={handle_Image} className='image'/>
+              {imageView && <img src={imageView} alt="미리보기" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />}
             </div>
           </div>
           <button type='submit'onClick={()=>navigate('/analyze_result')}>➤</button>
--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,5 +1,5 @@
 import './profile.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 
@@ -7,12 +7,28 @@ const Profile = () => {
 
   const navigate = useNavigate();
   const [babyName, setBabyName] = useState('김유진');
+  const [babyPercent, setBabyPercent] = useState(98);
+
+  useEffect(() => {
+    const saveBabyName = localStorage.getItem('babyName');
+    const saveBabyPercent = localStorage.getItem('babyPercent');
+
+    if(saveBabyName){
+      setBabyName(saveBabyName);
+    }
+    if(saveBabyPercent){
+      setBabyPercent(Number(saveBabyPercent));
+    }
+  },[]);
 
   const change_baby = () => {
-    if(window.confirm('정말 상대의 이름을 변경하시겠습니까?')){
+    if(window.confirm('정말 상대의 이름을 변경하시겠습니까? 분석 퍼센트가 사라집니다.')){
       const new_baby = prompt('상대의 이름을 입력해주세요');
       if(new_baby){
         setBabyName(new_baby);
+        setBabyPercent(0);
+        localStorage.setItem('babyName', new_baby);
+        localStorage.setItem('babyPercent', '0');
       }
     }
   }
@@ -29,7 +45,7 @@ const Profile = () => {
             <img src='/Vector.png' style={{width:'200px', height:'220px', backgroundColor:'white'}} alt='vector image'></img>
           </div>
         </div>
-        <h1 style={{color:'#E56767', alignSelf:'center'}}>0%</h1>
+        <h1 style={{color:'#E56767', alignSelf:'center'}}>{babyPercent}%</h1>
         <div className='baby' style={{display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <p style={{color:'#E56767', fontSize:'20px', fontWeight:'bold'}}>{babyName}</p>
           <div className='image' style={{backgroundColor:'white', width:'300px', height:'300px', borderRadius:'150px', padding:'10px', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
